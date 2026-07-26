0bfd4251aad8014990cb48e701342eaa751612a6
robsonsoaresid <robemsuave@gmail.com>
2026-07-26 14:20:41 -0300

    Cria o primeiro componente

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,21 @@ import "./App.css";
 
 // no react, componetes são FUNÇÕES
 
+function FormularioDeEvento (){
+  return (
+    <form className="form-evento">
+      <h2>
+        Preecha para criar um evnento:
+      </h2>
+      <fieldset>
+        <label htmlFor="nome">
+          Qual o nome do evnento?
+        </label>
+        <input type="text" id="nome" />
+      </fieldset>
+    </form>
+  );
+}
 
 function App() {
   return (
@@ -13,6 +28,7 @@ function App() {
       <section>
         <img src="/banner.png" alt="" />
       </section>
+      <FormularioDeEvento/>
     </main>
   );
 }
