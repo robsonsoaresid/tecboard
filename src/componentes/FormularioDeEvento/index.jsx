ce81111914cfd26e1abe560a7484bdbc7a06ebb7
robsonsoaresid <robemsuave@gmail.com>
2026-07-28 16:17:59 -0300

    Cria estilização de componentes, lista suspensa e botao

--- src/componentes/FormularioDeEvento/index.jsx
+++ src/componentes/FormularioDeEvento/index.jsx
@@ -1,21 +1,50 @@
-import './formulario-de-evento.estilos.css'
+import "./formulario-de-evento.estilos.css";
 import { CampoDeEntrada } from "../CampoDeEntrada";
 import { CampoDeFormulario } from "../CampoDeFormulario";
 import { Label } from "../Label";
-import {TituloFormulario} from "../TituloFormulario"
+import { TituloFormulario } from "../TituloFormulario";
+import { ListaSuspensa }  from "../ListaSuspensa";
+import { Botao } from "../Botao";
 
 export function FormularioDeEvento() {
   return (
     <form className="form-evento">
       <TituloFormulario>Preecha para criar um evnento:</TituloFormulario>
-      <CampoDeFormulario>
-        <Label htmlFor="nome">Qual o nome do evnento?</Label>
-        <CampoDeEntrada 
-        type="text" 
-        id="nome" 
-        placeholder="Summer dev hits" 
-        />
-      </CampoDeFormulario>
+      <div className="campos">
+        <CampoDeFormulario>
+          <Label htmlFor="nomeEvento">Qual o nome do evnento?</Label>
+          <CampoDeEntrada
+            type="text"
+            id="nomeEvento"
+            placeholder="Summer dev hits"
+            name="nomeEvento"
+          />
+        </CampoDeFormulario>
+        <CampoDeFormulario>
+          <Label htmlFor="dataEvento">Data do evento</Label>
+          <CampoDeEntrada
+            type="date"
+            id="dataEvento"
+           
+            name="dataEvento"
+          />
+        </CampoDeFormulario>
+        <CampoDeFormulario>
+          <Label htmlFor="htmlFor">Tema do evento</Label>
+          <ListaSuspensa 
+          name="tema" 
+          id="tema" 
+          defaultValue=""> 
+          <option value="" disabled>Selecione uma opção</option> 
+          </ListaSuspensa> 
+        </CampoDeFormulario>
+        
+      </div>
+      <div  className='acoes'>
+        <Botao>
+          Criar evento
+        </Botao>
+      </div>
     </form>
   );
 }
--- src/componentes/TituloFormulario/index.jsx
+++ src/componentes/TituloFormulario/index.jsx
@@ -1,3 +1,6 @@
+import './titulo-formulario.estilos.css'
+// props é um OBJETO
+// props.children
 export function TituloFormulario({ children }) {
-  return <h2>{children}</h2>;
+  return <h2 className='titulo-form'>{children}</h2>;
 }
